8d6a3b70e66cc8034db93652c6787e0d76dfb5e9
Rafael Gomes <rafarfl2010@gmail.com>
2024-11-12 20:52:26 -0300

    feat(styeles) define colors

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -3,18 +3,12 @@ import { Link } from 'react-router-dom';
 import VideoPlayer from '../components/VideoPlayer.tsx'
 
 const Videos: React.FC = () => {
-  const videos = [
-    { id: 1, title: 'Video 1', thumbnail: '/thumb1.jpg', videoUrl: '/videos/video1.mp4' },
-    { id: 2, title: 'Video 2', thumbnail: '/thumb2.jpg', videoUrl: '/videos/video2.mp4' },
-  ];
 
   const url = '../video.mp4'
 
   return (
-    <div>
-  
+    <div>  
         <VideoPlayer video={url} />
-  
     </div>
   );
 };
