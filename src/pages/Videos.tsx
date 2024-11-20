3400c8dc2518bb6bf8e81bdf91504f4b483e7de0
Rafael Gomes <rafarfl2010@gmail.com>
2024-11-19 22:26:25 -0300

    feat(media) pulling video from local api

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import VideoPlayer from '../components/VideoPlayer.tsx'
 import VideoPlayerv2 from '../components/VideoPlayerv2.tsx'
 
 const Videos: React.FC = () => {
+  const [videoSrc, setVideoSrc] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
-  const url = '../video.mp4'
+  useEffect(() => {
+    const fetchVideo = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/videos/1', {
+          responseType: 'blob',
+        });
+        const videoUrl = URL.createObjectURL(response.data);
+        setVideoSrc(videoUrl);
+      } catch (err) {
+        setError('Erro ao carregar o vídeo');
+      }
+    };
+
+    fetchVideo();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <div>
-        <VideoPlayerv2 src={url} poster="URL_DA_IMAGEM_DE_POSTER.jpg" />
+       {videoSrc ? (
+        <VideoPlayerv2 src={videoSrc} poster="URL_DA_IMAGEM_DE_POSTER.jpg" />
+       ) : (
+        <p>Carregando vídeo...</p>
+      )}
+        
     </div>
   );
 };
