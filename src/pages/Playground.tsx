f8bbfada9f761b41094382ddd275c089cdba1a56
Rafael Gomes <rafarfl2010@gmail.com>
2024-12-14 13:20:46 -0300

    feat(component): replace thumbnail

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import VideoPlayerHSL from '../components/VideoPlayerHSL.tsx'
 import VideoList from '../components/VideoList.tsx'
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'
+import thumbnail from '@root/assets/daima.jpg'
+
 
 const Playground: React.FC = () => {
   const [videoSrc, setVideoSrc] = useState<string>('')
@@ -15,25 +17,25 @@ const Playground: React.FC = () => {
     {
       id: 1,
       title: 'Card 1',
-      thumbnail: 'https://via.placeholder.com/300x150',
+      thumbnail,
       link: '/videos/animes/dragonball-daima/01/ep1',
     },
     {
       id: 2,
       title: 'Card 2',
-      thumbnail: 'https://via.placeholder.com/300x150',
+      thumbnail,
       link: '/videos/animes/dragonball-daima/01/ep2',
     },
     {
       id: 3,
       title: 'Card 3',
-      thumbnail: 'https://via.placeholder.com/300x150',
+      thumbnail,
       link: '/videos/animes/dragonball-daima/01/ep3',
     },
     {
       id: 4,
       title: 'Card 4',
-      thumbnail: 'https://via.placeholder.com/300x150',
+      thumbnail,
       link: '/videos/animes/dragonball-daima/01/ep4',
     },
   ]
@@ -46,15 +48,15 @@ const Playground: React.FC = () => {
       } catch (err) {
         setError('Erro ao carregar o vídeo')
       } */
-      setVideoSrc(`/api/videos/animes/dragonball-daima/${id}/Dragon-Ball-Daima-Legendado.m3u8`)
+        setVideoSrc(`/api/videos/animes/dragonball-daima/${id}/Dragon-Ball-Daima-Legendado.m3u8`)
     };
 
     fetchVideo();
   }, [id]);
 
-  if (error) {
+  /* if (error) {
     return <div>{error}</div>;
-  }
+  } */
 
   return (
       <>
